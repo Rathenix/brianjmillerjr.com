62b9d0db04598d84ed67b8f72967917a6b38c48a
Brian J Miller Jr <brian.j.miller.jr@gmail.com>
2017-11-07 21:54:08 -0800

    Minor tweaks

--- src/header.js
+++ src/header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './header.css';
 
 export class Header extends Component {
 	render() {
 		return (
 			<div className="header">
-				<h1>Welcome to Brian J Miller Jr's Excellent Website</h1>
-				<p>A cool place where I will probably put links to stuff that relates to my life</p>
+				<div className="headerText">Welcome to Brian J Miller Jr's Excellent Website</div>
+				<span className="headerSubText">A cool place where I will probably put links to stuff that relates to my life</span>
 			</div>
 		);
 	}
